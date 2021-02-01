92eef6d2789dd6b7920e18a9888addaad27ee23f
Nicolas Campero <nicolas.jc1017@gmail.com>
2021-02-01 12:33:06 -0600

    updated 2-19

--- Homework1/ZyLabs_2-19.py
+++ Homework1/ZyLabs_2-19.py
@@ -12,16 +12,19 @@ print('How many servings does this make?')
 servings = int(input())
 
 # output amount of ingredients for x amount of servings
-print('\nLemonade ingredients - yields', '{:.2f}'.format(servings), 'servings')
+print('\n', end='')
+print('Lemonade ingredients - yields', '{:.2f}'.format(servings), 'servings')
 print('{:.2f}'.format(l_juice_cups), 'cup(s) lemon juice')
 print('{:.2f}'.format(water_cups), 'cup(s) water')
 print('{:.2f}'.format(agave_cups), 'cup(s) agave nectar')
 
+print('\n', end='')
 print('How many servings would you like to make?')
 servings_to_make = float(input())
 
 # adjust amounts of each ingredient to match servings amount
-print('\nLemonade ingredients - yields', '{:.2f}'.format(servings_to_make), 'servings')
+print('\n', end='')
+print('Lemonade ingredients - yields', '{:.2f}'.format(servings_to_make), 'servings')
 multiple_of_servings = float(servings_to_make / servings)
 lemon_after_servings = l_juice_cups * multiple_of_servings
 water_after_servings = water_cups * multiple_of_servings
@@ -34,6 +37,7 @@ print('{:.2f}'.format(agave_after_servings), 'cup(s) agave nectar')
 lemon_gallons = lemon_after_servings / 16.00
 water_gallons = water_after_servings / 16.00
 agave_gallons = agave_after_servings / 16.00
+print('\n', end='')
 print('Lemonade ingredients- yields', '{:.2f}'.format(servings_to_make), 'servings')
 print('{:.2f}'.format(lemon_gallons), 'gallon(s) lemon juice')
 print('{:.2f}'.format(water_gallons), 'gallon(s) water')
