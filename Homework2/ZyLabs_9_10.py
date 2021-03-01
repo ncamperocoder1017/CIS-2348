3d1d30e9cf69cac8b9febc06699d8b1fab7303a4
Nicolas Campero <nicolas.jc1017@gmail.com>
2021-03-01 16:07:24 -0600

    added HW 2 file

---
+++ Homework2/ZyLabs_9_10.py
@@ -0,0 +1,17 @@
+# Nicolas Campero
+# 1856853
+
+import csv
+file = input()
+word_freq = {}
+
+with open(file, 'r') as csvfile:
+    csvfile = csv.reader(csvfile)
+    for row in csvfile:
+        for word in row:
+            if word not in word_freq.keys():
+                word_freq[word] = 1
+            else:
+                word_freq[word] = word_freq[word] + 1
+for i in word_freq.keys():
+    print(i, word_freq[i])
