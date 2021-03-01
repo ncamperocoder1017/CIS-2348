e4f27704cda238c0a71e3a897669c7ee0c6011e7
Nicolas Campero <nicolas.jc1017@gmail.com>
2021-03-01 14:08:27 -0600

    added HW 2 file

--- Homework2/Date_Format_Operations_HW2.py
+++ Homework2/Date_Format_Operations_HW2.py
@@ -21,23 +21,6 @@ in_file = open('inputDates.txt', 'r')
 out_file = open('parsedDates.txt', 'w')
 
 
-for date in in_file:
-    if date != "-1":  # if the list element = -1 then do not add to date.split() list
-        full_list = date.split()
-        if len(full_list) >= 3:  # if length of full_list is 3 or more,  make first element month, 2nd day, 3rd year
-            month = full_list[0]
-            day = full_list[1]
-            year = full_list[2]
-
-            if month in date_list:
-                char = day[-1]
-                if char == ',':
-                    day = day[:len(day)-1]
-                    month_num = date_list[month]
-                    answer = month_num + '/' + day + '/' + year
-
-                    out_file.write(answer)  # write all output to new file "parsedDates"
-                    out_file.write('\n')  # write a new line to the end of parsedDates file
 
 out_file.close()  # close the parsedDates.txt file
 in_file.close()  # close the inputDates.txt file
