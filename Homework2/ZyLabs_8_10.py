ef70360fd785e3e3a971f586b4425cf5a1be642b
Nicolas Campero <nicolas.jc1017@gmail.com>
2021-03-01 16:18:16 -0600

    added HW 2 file

--- Homework2/ZyLabs_8_10.py
+++ Homework2/ZyLabs_8_10.py
@@ -3,9 +3,9 @@
 
 # get string input from user
 pal_word = input()
-pal_word = pal_word.replace(' ', '')  # Replace any spaces to test for palindromes in multiple-word phrases
+new_word = pal_word.replace(' ', '')  # Replace any spaces to test for palindromes in multiple-word phrases
 
-if pal_word == pal_word[::-1]:
+if new_word == new_word[::-1]:
     print(pal_word, "is a palindrome")
 else:
     print(pal_word, "is not a palindrome")
